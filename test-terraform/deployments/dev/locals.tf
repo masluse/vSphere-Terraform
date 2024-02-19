081d297dbdbb0598c327f37e7bc9f37f40514ce0
masluse <122784119+masluse@users.noreply.github.com>
2024-02-19 13:43:55 +0000

    Test

--- test-terraform/deployments/dev/locals.tf
+++ test-terraform/deployments/dev/locals.tf
@@ -8,18 +8,18 @@ locals {
   datacenter = "Main" # Name of the Datacenter
 
   # variables for the virtual machine
-  vm_host = ["10.27.9.3"] # ESXi Host
-  vm_type = ["Linux"] # Linux or Windows
-  vm_name         = ["mansrv92010"] # Name of the VM
-  vm_datastore    = ["Synology"] # Name of the Datastore
-  vm_network      = ["School"] # Name of the Network
-  vm_template     = ["Ubuntu Server 22.04"] # Name of the Template
-  vm_memory       = [4] # Memory in GB
-  vm_num_cpus     = [4] # Number of CPUs
-  vm_ipv4_address = ["172.16.3.2"] # Static IP Address
-  vm_ipv4_netmask = [24] # Subnet Mask
-  vm_ipv4_gateway = ["172.16.3.1"] # Default Gateway
-  vm_ipv4_dns     = ["172.16.3.1"] # DNS Server
-  vm_folder       = ["/Linux/Ubuntu"] # Folder in which to place the VM
-  vm_annotation   = ["Test Server for Terraform"] # Annotation for the VM
+  vm_host         = ["10.27.9.3", "10.27.9.3"]                                 # ESXi Host
+  vm_type         = ["Linux", "Windows"]                                       # Linux or Windows
+  vm_name         = ["mansrv92010", "mansrv92011"]                             # Name of the VM
+  vm_datastore    = ["Synology", "Synology"]                                   # Name of the Datastore
+  vm_network      = ["School", "School"]                                       # Name of the Network
+  vm_template     = ["Ubuntu Server 22.04", "Windows Server 2022"]             # Name of the Template
+  vm_memory       = [4, 4]                                                     # Memory in GB
+  vm_num_cpus     = [4, 4]                                                     # Number of CPUs
+  vm_ipv4_address = ["172.16.3.2", "172.16.3.3"]                               # Static IP Address
+  vm_ipv4_netmask = [24, 24]                                                   # Subnet Mask
+  vm_ipv4_gateway = ["172.16.3.1", "172.16.3.1"]                               # Default Gateway
+  vm_ipv4_dns     = ["172.16.3.1", "172.16.3.1"]                               # DNS Server
+  vm_folder       = ["/Linux/Ubuntu", "/Windows/Windows Server 2022"]          # Folder in which to place the VM
+  vm_annotation   = ["Test Server for Terraform", "Test Server for Terraform"] # Annotation for the VM
 }
--- test-terraform/deployments/dev/modules.tf
+++ test-terraform/deployments/dev/modules.tf
@@ -6,22 +6,22 @@
 ### Create a Virtual Machine
 ##############################################
 module "virtual_machine" {
-  count        = length(local.vm_name)
-  source       = "../../modules/virtual_machine"
-  datacenter   = local.datacenter                   # Datacenter where the VM will be deployed
-  datastore    = local.vm_datastore[count.index]    # Datastore where the VM will be deployed
-  host         = local.vm_host[count.index]         # Host where the VM will be deployed
-  network      = local.vm_network[count.index]      # Network where the VM will be deployed
-  template     = local.vm_template[count.index]     # Template to use for the VM
-  vm_name      = local.vm_name[count.index]         # Name of the VM
-  num_cpus     = local.vm_num_cpus[count.index]     # Number of CPUs for the VM
-  memory       = local.vm_memory[count.index] * 1024       # Memory for the VM
-  ipv4_address = local.vm_ipv4_address[count.index] # IPv4 address for the VM
-  ipv4_netmask = local.vm_ipv4_netmask[count.index] # IPv4 netmask for the VM
-  ipv4_gateway = local.vm_ipv4_gateway[count.index] # IPv4 gateway for the VM
-  ipv4_dns     = local.vm_ipv4_dns[count.index]     # IPv4 DNS for the VM
-  folder       = local.vm_folder[count.index]       # Folder where the VM will be deployed
-  annotation   = local.vm_annotation[count.index]   # Annotation for the VM
-  type = local.vm_type[count.index]                 # Type of the VM
-  admin_password = var.vsphere_password             # Password for the VM
+  count          = length(local.vm_name)
+  source         = "../../modules/virtual_machine"
+  datacenter     = local.datacenter                    # Datacenter where the VM will be deployed
+  datastore      = local.vm_datastore[count.index]     # Datastore where the VM will be deployed
+  host           = local.vm_host[count.index]          # Host where the VM will be deployed
+  network        = local.vm_network[count.index]       # Network where the VM will be deployed
+  template       = local.vm_template[count.index]      # Template to use for the VM
+  vm_name        = local.vm_name[count.index]          # Name of the VM
+  num_cpus       = local.vm_num_cpus[count.index]      # Number of CPUs for the VM
+  memory         = local.vm_memory[count.index] * 1024 # Memory for the VM
+  ipv4_address   = local.vm_ipv4_address[count.index]  # IPv4 address for the VM
+  ipv4_netmask   = local.vm_ipv4_netmask[count.index]  # IPv4 netmask for the VM
+  ipv4_gateway   = local.vm_ipv4_gateway[count.index]  # IPv4 gateway for the VM
+  ipv4_dns       = local.vm_ipv4_dns[count.index]      # IPv4 DNS for the VM
+  folder         = local.vm_folder[count.index]        # Folder where the VM will be deployed
+  annotation     = local.vm_annotation[count.index]    # Annotation for the VM
+  type           = local.vm_type[count.index]          # Type of the VM
+  admin_password = var.vsphere_password                # Password for the VM
 }
