b6605691c8d28b30a093f8d73c41749b90a648ba
masluse <122784119+masluse@users.noreply.github.com>
2024-02-19 12:09:39 +0000

    test

--- test-terraform/deployments/dev/locals.tf
+++ test-terraform/deployments/dev/locals.tf
@@ -1,24 +1,22 @@
 # Define local variables for reuse within this Terraform module.
 
 locals {
-    # variables for the provider
-    vsphere_server = "vcenter.teleport.mregli.com"
+  # variables for the provider
+  vsphere_server = "vcenter.teleport.mregli.com"
 
-    # variables for the datacenter
-    datacenter = "Main"
+  # variables for the datacenter
+  datacenter = "Main"
 
-    # variables for the server
-    host = "10.27.9.3"
-
-    # variables for the virtual machine
-    vm_name = "mansrv92010"
-    vm_datastore = "Synology"
-    vm_network = "School"
-    vm_template = "Ubuntu Server 22.04"
-    vm_memory = 4096
-    vm_num_cpus = 4
-    vm_ipv4_address = "172.16.3.2"
-    vm_ipv4_netmask = 24
-    vm_ipv4_gateway = "172.16.3.1"
-    vm_ipv4_dns = ["172.16.3.1"]
+  # variables for the virtual machine
+  vm_host = ["10.27.9.3"]
+  vm_name         = ["mansrv92010"]
+  vm_datastore    = ["Synology"]
+  vm_network      = ["School"]
+  vm_template     = ["Ubuntu Server 22.04"]
+  vm_memory       = [4]
+  vm_num_cpus     = [4]
+  vm_ipv4_address = ["172.16.3.2"]
+  vm_ipv4_netmask = [24]
+  vm_ipv4_gateway = ["172.16.3.1"]
+  vm_ipv4_dns     = ["172.16.3.1"]
 }
--- test-terraform/deployments/dev/modules.tf
+++ test-terraform/deployments/dev/modules.tf
@@ -6,17 +6,18 @@
 ### Create a Virtual Machine
 ##############################################
 module "virtual_machine" {
-    source = "../../modules/virtual_machine"
-    datacenter = local.datacenter
-    datastore = local.vm_datastore
-    host = local.host
-    network = local.vm_network
-    template = local.vm_template
-    vm_name = local.vm_name
-    num_cpus = local.vm_num_cpus
-    memory = local.vm_memory
-    ipv4_address = local.vm_ipv4_address
-    ipv4_netmask = local.vm_ipv4_netmask
-    ipv4_gateway = local.vm_ipv4_gateway
-    ipv4_dns = local.vm_ipv4_dns
+  count        = length(local.vm_name)
+  source       = "../../modules/virtual_machine"
+  datacenter   = local.datacenter                   # Datacenter where the VM will be deployed
+  datastore    = local.vm_datastore[count.index]    # Datastore where the VM will be deployed
+  host         = local.vm_host[count.index]         # Host where the VM will be deployed
+  network      = local.vm_network[count.index]      # Network where the VM will be deployed
+  template     = local.vm_template[count.index]     # Template to use for the VM
+  vm_name      = local.vm_name[count.index]         # Name of the VM
+  num_cpus     = local.vm_num_cpus[count.index]     # Number of CPUs for the VM
+  memory       = local.vm_memory[count.index] * 1024       # Memory for the VM
+  ipv4_address = local.vm_ipv4_address[count.index] # IPv4 address for the VM
+  ipv4_netmask = local.vm_ipv4_netmask[count.index] # IPv4 netmask for the VM
+  ipv4_gateway = local.vm_ipv4_gateway[count.index] # IPv4 gateway for the VM
+  ipv4_dns     = local.vm_ipv4_dns[count.index]     # IPv4 DNS for the VM
 }
